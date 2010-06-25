b0db5b591f313582f6d943ee4869c9fb6dfefef7
steve.swinsburg@gmail.com <steve.swinsburg@gmail.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-06-25 08:01:51 +0000

    SAK-18044 Add -Dhttp.agent=Sakai to demo startup scripts to resolve Google RSS feed issues - 2.5.x fix
    git-svn-id: https://source.sakaiproject.org/svn/reference/branches/sakai_2-5-x@78744 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- demo/start-sakai.sh
+++ demo/start-sakai.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-JAVA_OPTS="-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true"
+JAVA_OPTS="-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dhttp.agent=Sakai -Dsakai.demo=true"
 export JAVA_OPTS
 
-CATALINA_OPTS="-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true"
+CATALINA_OPTS="-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dhttp.agent=Sakai -Dsakai.demo=true"
 export CATALINA_OPTS
 
 # Demo failed to start correctly if CATALINA_HOME / CATALINA_BASE is set SAK-11553
