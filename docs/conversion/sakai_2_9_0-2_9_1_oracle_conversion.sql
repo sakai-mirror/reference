a95fced6a702d5085fb61f2c4c9cb82d5cec802a
ottenhoff@longsight.com <ottenhoff@longsight.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2012-12-06 14:28:23 +0000

    SAK-22946 merge r111808,111822 from trunk
    git-svn-id: https://source.sakaiproject.org/svn/reference/branches/sakai-2.9.x@117292 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ docs/conversion/sakai_2_9_0-2_9_1_oracle_conversion.sql
@@ -0,0 +1,13 @@
+
+-- -----------------------------------------------------------------------
+-- SAK-22496
+-- -----------------------------------------------------------------------
+
+UPDATE CITATION_SCHEMA_FIELD SET PROPERTY_VALUE = 'BT,T2' WHERE SCHEMA_ID = 'chapter' AND FIELD_ID = 'sourceTitle' AND PROPERTY_NAME = 'sakai:ris_identifier';
+UPDATE CITATION_SCHEMA_FIELD SET PROPERTY_VALUE = 'BT,T2' WHERE SCHEMA_ID = 'proceed' AND FIELD_ID = 'sourceTitle' AND PROPERTY_NAME = 'sakai:ris_identifier';
+UPDATE CITATION_SCHEMA_FIELD SET PROPERTY_VALUE = 'JF,JO,JA,J1,J2,BT,T2' WHERE SCHEMA_ID = 'article' AND FIELD_ID = 'sourceTitle' AND PROPERTY_NAME = 'sakai:ris_identifier';
+
+-- -----------------------------------------------------------------------
+-- end SAK-22496
+-- -----------------------------------------------------------------------
+
