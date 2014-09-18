fa873fd9d93d496fc79f05af5770440eab1bb9dc
enietzel@anisakai.com <enietzel@anisakai.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2014-09-18 15:33:41 +0000

    SAK-27733 merge 311220 to 10.x
    git-svn-id: https://source.sakaiproject.org/svn/reference/branches/sakai-10.x@313705 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- library/src/webapp/js/lang-datepicker/lang-datepicker.js
+++ library/src/webapp/js/lang-datepicker/lang-datepicker.js
@@ -2094,6 +2094,10 @@ isRTL:false
 		cfg.showTimepicker = options.useTime;
 		cfg.parseFormat = options.parseFormat;
 
+		//Enable the dropdowns for month and year SAK-27733
+		cfg.changeMonth = true;
+		cfg.changeYear = true;
+
 		// disable the input field on show as to not display a tablet keyboard
 		cfg.onClose = function(dateText, inst) {
 			$(this).removeProp("disabled");
